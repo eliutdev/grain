fea1f51a5483a3fc0bcd4b520cc45f1981d93513
reliut-g <reliutg@gmail.com>
2021-09-03 13:16:39 +0200

    feat: page for tag

---
+++ pages/tags/_name.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="section">
+    <h1 class="title is-1"><span class="tag is-large">#</span> {{ tag }}</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Tag',
+  asyncData({ params }) {
+    return {
+      tag: params.name,
+    }
+  },
+}
+</script>
