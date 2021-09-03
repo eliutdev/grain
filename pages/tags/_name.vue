8a28525e93f8260fdad4898ed0cef69e299fd35d
reliut-g <reliutg@gmail.com>
2021-09-03 13:36:52 +0200

    feat: show ideas by tag

--- pages/tags/_name.vue
+++ pages/tags/_name.vue
@@ -1,16 +1,32 @@
 <template>
   <section class="section">
     <h1 class="title is-1"><span class="tag is-large">#</span> {{ tag }}</h1>
+    <p>{{ matchText }}</p>
+    <hr />
+    <List :items="ideas" />
   </section>
 </template>
 
 <script>
 export default {
-  name: 'Tag',
-  asyncData({ params }) {
+  name: 'TagExplorer',
+  async asyncData({ params, $content }) {
+    const ideas = await $content({ deep: true })
+      .where({ tags: { $contains: [params.name] } })
+      .fetch()
     return {
-      tag: params.name,
+      ideas, // ideas is an array of objects
+      tag: params.name, // current tag
     }
+  },
+  computed: {
+    matchText() {
+      return this.ideas.length === 0
+        ? 'No ideas found'
+        : this.ideas.length === 1
+        ? '1 idea found'
+        : `Here are ${this.ideas.length} ideas found`
+    },
   },
 }
 </script>
