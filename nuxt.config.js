fe81c85ce72127a8482b5e54c5a45cb62d2477a9
reliut-g <reliutg@gmail.com>
2021-09-03 12:24:21 +0200

    chore: generate routes

--- nuxt.config.js
+++ nuxt.config.js
@@ -59,6 +59,15 @@ export default {
     },
   },
 
+  generate: {
+    async routes() {
+      const { $content } = require('@nuxt/content')
+      const files = await $content({ deep: true }).only(['path']).fetch()
+
+      return files.map((file) => (file.path === '/index' ? '/' : file.path))
+    },
+  },
+
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
 }
