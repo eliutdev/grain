e204031173d57d84ad95779418fc7a271509507f
reliut-g <reliutg@gmail.com>
2021-09-04 12:39:58 +0200

    feat: show top tags and tiers

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,25 @@
 <template>
-  <div class="container is-fluid">
+  <div class="container">
     <Nav />
     <main class="columns is-desktop">
-      <div class="column is-one-quarter"></div>
+      <div class="column is-one-quarter">
+        <h2 class="subtitle is-4">Tiers</h2>
+        <ul>
+          <li
+            v-for="(level, index) in levels"
+            :key="level.slug"
+            :class="{ 'mb-5': index === levels.length - 1 }"
+          >
+            <nuxt-link :to="`/ideas/${level.slug}`">{{ level.name }}</nuxt-link>
+          </li>
+        </ul>
+        <h2 class="subtitle is-4">Top tags</h2>
+        <ul>
+          <li v-for="tag in topTags" :key="tag">
+            <nuxt-link :to="`/tags/${tag}`"> #{{ tag }} </nuxt-link>
+          </li>
+        </ul>
+      </div>
       <div class="column">
         <Nuxt />
       </div>
@@ -22,5 +39,28 @@
 <script>
 export default {
   name: 'Layout',
+  data() {
+    return {
+      levels: [
+        {
+          name: 'Beginner',
+          slug: 'beginner',
+        },
+        {
+          name: 'Intermediate',
+          slug: 'intermediate',
+        },
+        {
+          name: 'Advanced',
+          slug: 'advanced',
+        },
+      ],
+    }
+  },
+  computed: {
+    topTags() {
+      return this.$store.getters.topTags
+    },
+  },
 }
 </script>
