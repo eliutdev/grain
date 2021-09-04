76a290b0d323f2f7c30d38fa918a5fcb0068b1ea
reliut-g <reliutg@gmail.com>
2021-09-04 13:09:32 +0200

    feat: get an inspirational quote

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,13 @@
 <template>
   <section>
-    <h1 class="title">"Start with a grain of sand, grow into a mountain."</h1>
+    <h1 class="title">Ideas</h1>
+    <blockquote :cite="quote.url">
+      <p>{{ quote.text }}</p>
+      <figcaption>
+        <a :href="quote.url" target="_blank">—{{ quote.author }}</a>
+      </figcaption>
+    </blockquote>
+    <hr />
     <List :items="ideas" />
   </section>
 </template>
@@ -8,6 +15,26 @@
 <script>
 export default {
   name: 'Home',
+  data() {
+    return {
+      quote: {
+        author: '',
+        text: '',
+        url: '',
+      },
+    }
+  },
+  async fetch() {
+    const url = 'https://quotes.rest/qod?category=inspire&language=en'
+    const result = await fetch(url)
+    const data = await result.json()
+    this.quote = {
+      author: data.contents.quotes[0].author,
+      text: data.contents.quotes[0].quote,
+      url: data.contents.quotes[0].permalink,
+    }
+  },
+  fetchOnServer: false,
   computed: {
     ideas() {
       return this.$store.state.ideas
